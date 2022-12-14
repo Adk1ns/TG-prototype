a4185ddef8739c21c5ca80e55cf942011338c16f
Jered Adkins <jeredkins@gmail.com>
2022-12-13 17:17:51 -0800

    peerjs basics

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import GlobalStyles from './components/GlobalStyles'
 import NavbarTG from './components/nav/NavbarTG'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Meeting from './pages/Meeting'
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="meeting" element={<p>meeting</p>} />
+            <Route path="meeting" element={<Meeting />} />
             <Route path="planner" element={<p>project planner</p>} />
             <Route path="explore" element={<p>explore groups</p>} />
             <Route path="messages" element={<p>messages</p>} />
