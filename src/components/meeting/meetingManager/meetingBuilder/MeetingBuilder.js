dec55e6e30197f6a986b3d8b3e5a3d37e764716e
Jered Adkins <jeredkins@gmail.com>
2022-12-27 10:05:01 -0800

    meeting builder start

---
+++ src/components/meeting/meetingManager/meetingBuilder/MeetingBuilder.js
@@ -0,0 +1,39 @@
+import { Button } from '@mui/material'
+
+const MeetingBuilder = ({ meetingInProgress, setMeetingInProgress }) => {
+  console.log(meetingInProgress)
+
+  const StartMeeting = () => {
+    setMeetingInProgress((prev) => !prev)
+  }
+
+  return (
+    <div className="outline p-3">
+      <h5>Select meeting components</h5>
+      <div className="outline p-3">
+        <h5>Questionnaire</h5>
+        <input placeholder="Question" />
+        <button>+</button>
+      </div>
+      <div className="outline p-3">
+        <h5>Poll</h5>
+        <input placeholder="Option" />
+        <button>+</button>
+      </div>
+      <div className="outline p-3">
+        <h5>Quiz</h5>
+        <input placeholder="Question" />
+        <input placeholder="Answer" />
+        <button>+</button>
+      </div>
+      <Button variant="contained" color="primary" onClick={StartMeeting}>
+        Schedule
+      </Button>
+      <Button variant="contained" color="success" onClick={StartMeeting}>
+        Start
+      </Button>
+    </div>
+  )
+}
+
+export default MeetingBuilder
--- src/pages/Meeting.js
+++ src/pages/Meeting.js
@@ -1,27 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import VideoRoom from '../components/meeting/videoRoom/VideoRoom'
 import { atom, useAtom } from 'jotai'
 import { exampleAtom } from '../components/store/Atoms'
 import MeetingManager from '../components/meeting/meetingManager/MeetingManager'
+import { Button } from '@mui/material'
+import MeetingBuilder from '../components/meeting/meetingManager/meetingBuilder/MeetingBuilder'
 
 //will contain video room and meeting manager
 
 const Meeting = () => {
   const [text, setText] = useAtom(exampleAtom)
+  const [meetingInProgress, setMeetingInProgress] = useState(false)
+
+  const StartMeeting = () => {
+    setMeetingInProgress((prev) => !prev)
+  }
 
   return (
     <div className="col">
-      <div className="row g-0">
-        <div className="col-12 col-sm-6 p-3">
-          <h5>Video Room</h5>
-          <VideoRoom />
+      {meetingInProgress && (
+        <div className="row g-0">
+          <div className="col-12 col-sm-6 p-3">
+            <h5>Video Room</h5>
+            <VideoRoom />
+            <Button variant="contained" color="error" onClick={StartMeeting}>
+              End
+            </Button>
+          </div>
+          <div className="col-12 col-sm-6 p-3">
+            {' '}
+            {/* <p>Jotai text: {text}</p> */}
+            <MeetingManager />
+          </div>
         </div>
-        <div className="col-12 col-sm-6 p-3">
-          {' '}
-          {/* <p>Jotai text: {text}</p> */}
-          <MeetingManager />
+      )}
+      {!meetingInProgress && (
+        <div className="p-5">
+          <p>build a meeting</p>
+          <MeetingBuilder
+            meetingInProgress={meetingInProgress}
+            setMeetingInProgress={setMeetingInProgress}
+          />
         </div>
-      </div>
+      )}
     </div>
   )
 }
